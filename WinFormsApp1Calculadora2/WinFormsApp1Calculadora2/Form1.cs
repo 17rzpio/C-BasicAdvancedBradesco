da330303320f35ea495bde5f18e4ece1b3a428cd
17rzpio <74218387+17rzpio@users.noreply.github.com>
2024-08-15 15:43:33 -0300

    Add files via upload
    curso de c# básico e avançado da Escola Virtual Bradesco

---
+++ WinFormsApp1Calculadora2/WinFormsApp1Calculadora2/Form1.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1Calculadora2
+{
+    
+    public partial class Form1 : Form
+    {
+        float valor1=0;
+        public int digito = 0;
+        string display;
+        Boolean valorInicial = true;
+        Boolean valorInicialmenos = true;
+        Boolean valorInicialvezes = true;
+        Boolean valorInicialdivisao = true;
+        Boolean valorInicialresto = true;
+        float valor3=0;
+        public int valorDigitado(int valor2)
+        {
+            int valor4;
+            if (digito == 0)
+            {
+
+                txtResposta.Text = valor2.ToString();
+                digito++;
+                return (valor2);
+                
+            } else if (digito == 7)
+            {
+                valor4 = (int)valor1;
+                return (valor4);
+            }
+            valor1 *= 10;
+            valor4=(int)valor1;
+            valor2 += valor4;
+            valor1 = valor2;
+            txtResposta.Text= valor2.ToString();
+            digito++;
+            return (valor2);
+        }
+        public void operacao(string operador)
+        {
+            
+            
+                if (operador == "soma") {
+                if (valorInicial == true)
+                {
+                    valorInicialmenos = true;
+                    valorInicialvezes = true;
+                    valorInicialdivisao = true;
+                    valorInicialresto = true;
+                    valorInicial = false;
+                    valor3 = valor1;
+                    digito = 0;
+                }
+                else
+                {
+                    valor1 += valor3;
+                    valorInicial = true;
+                }
+                }
+                if (operador == "menos")
+                {
+                if (valorInicialmenos == true)
+                {
+                    valorInicial = true;
+                    valorInicialvezes = true;
+                    valorInicialdivisao = true;
+                    valorInicialresto = true;
+                    valorInicialmenos = false;
+                    valor3 = valor1;
+                    digito = 0;
+                }
+                else
+                {
+                    valor3 -= valor1;
+                    valor1 = valor3;
+                    valorInicialmenos = true;
+                }
+                }
+                if (operador == "vezes")
+                {
+                if (valorInicialvezes == true)
+                {
+                    valorInicialmenos = true;
+                    valorInicial = true;
+                    valorInicialdivisao = true;
+                    valorInicialresto = true;
+                    valorInicialvezes = false;
+                    valor3 = valor1;
+                    digito = 0;
+                }
+                else
+                {
+                    valor1 *= valor3;
+
+                    valorInicialvezes = true;
+                }
+                }
+                if (operador == "divisao")
+                {
+                if (valorInicialdivisao == true)
+                {
+                    valorInicialmenos = true;
+                    valorInicialvezes = true;
+                    valorInicial = true;
+                    valorInicialresto = true;
+                    valorInicialdivisao = false;
+                    valor3 = valor1;
+                    digito = 0;
+                }
+                else
+                {
+                    valor3 /= valor1;
+                    valor1 = valor3;
+
+                    valorInicialdivisao = true;
+                }
+                }
+                if (operador == "resto")
+                {
+                if (valorInicialresto == true)
+                {
+                    valorInicialmenos = true;
+                    valorInicialvezes = true;
+                    valorInicialdivisao = true;
+                    valorInicial = true;
+                    valorInicialresto = false;
+                    valor3 = valor1;
+                    digito = 0;
+                }
+                else
+                {
+                    valor3 %= valor1;
+                    valor1 = valor3;
+
+                    valorInicialresto = true;
+                }
+                }
+                txtResposta.Text = valor1.ToString();
+                valor3 = valor1;
+                valor1 = 0;
+                digito = 0;
+            
+        }
+
+public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+
+            valor1 = valorDigitado(1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            valor1 = valorDigitado(2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            valor1 = valorDigitado(3);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            valor1 = valorDigitado(4);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            valor1 = valorDigitado(5);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            valor1 = valorDigitado(6);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            valor1 = valorDigitado(7);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            valor1 = valorDigitado(8);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            valor1 = valorDigitado(9);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            valor1 = valorDigitado(0);
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            operacao("soma");
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            if (digito != 0)
+            {
+                operacao("menos");
+            }
+            
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            if (digito != 0)
+            {
+                operacao("vezes");
+            }
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            if (digito != 0)
+            {
+                operacao("divisao");
+            }
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            if (digito != 0)
+            {
+                operacao("resto");
+            }
+        }
+
+        private void button27_Click(object sender, EventArgs e)
+        {
+            if (digito == 0)
+            {
+                txtResposta.Text = valor1.ToString();
+
+            }
+            if(digito!=0)
+            {
+                if (valorInicial == false)
+                {
+                    valor1 += valor3;
+                    txtResposta.Text = valor1.ToString();
+                    valor3 = valor1;
+                    valor1 = 0;
+                    digito = 0;
+                    valorInicial = true;
+                }
+                if (valorInicialmenos == false)
+                {
+
+                    valor3 -= valor1;
+                    valor1 = valor3;
+                    txtResposta.Text = valor1.ToString();
+                    valor3 = valor1;
+                    valor1 = 0;
+                    digito = 0;
+                    valorInicialmenos = true;
+                }
+                if (valorInicialvezes == false)
+                {
+                    valor1 *= valor3;
+                    txtResposta.Text = valor1.ToString();
+                    valor3 = valor1;
+                    valor1 = 0;
+                    digito = 0;
+                    valorInicialvezes = true;
+
+                }
+                if (valorInicialdivisao == false)
+                {
+                    valor3 /= valor1;
+                    valor1 = valor3;
+                    txtResposta.Text = valor1.ToString();
+                    valor3 = valor1;
+                    valor1 = 0;
+                    digito = 0;
+                    valorInicialdivisao = true;
+
+                }
+                if (valorInicialresto == false)
+                {
+
+                    valor3 %= valor1;
+                    valor1 = valor3;
+                    txtResposta.Text = valor1.ToString();
+                    valor3 = valor1;
+                    valor1 = 0;
+                    digito = 0;
+                    valorInicialresto = true;
+                }
+            }
+        }
+    }
+}
---
+++ WinFormsApp1CriarForm/WinFormsApp1CriarForm/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿
+namespace WinFormsApp1CriarForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button3.Font = new System.Drawing.Font("Showcard Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(177, 358);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 23;
+            this.button3.Text = "Novo";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(360, 358);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 22;
+            this.button2.Text = "Salvar";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(575, 358);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "Sair";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(158, 291);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(527, 23);
+            this.textBox4.TabIndex = 20;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(158, 250);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(527, 23);
+            this.textBox3.TabIndex = 19;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(158, 196);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(527, 23);
+            this.textBox2.TabIndex = 18;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(177, 138);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(527, 23);
+            this.textBox1.TabIndex = 17;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(96, 294);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 15);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Cidade";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(96, 250);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 15);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Telefone";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(96, 196);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 15);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Endereço";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(96, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 15);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Nome";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 29.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(319, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(229, 52);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "CADASTRO";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button4.Font = new System.Drawing.Font("Segoe UI Symbol", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button4.Location = new System.Drawing.Point(560, 464);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(107, 41);
+            this.button4.TabIndex = 24;
+            this.button4.Text = "Mensagem";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(320, 478);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 25;
+            this.button5.Text = "clique aqui";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(310, 449);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 23);
+            this.txtNome.TabIndex = 26;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 533);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Novo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox txtNome;
+    }
+}
+
